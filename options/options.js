9d797c037a675c0db15b810038711b8fd6f33077
Crowdbotics <team@crowdbotics.com>
2023-11-19 09:04:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cash-grab-44667",
+  url: "https://cash-grab-44667.botics.co",
+  api: "https://cash-grab-44667.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
